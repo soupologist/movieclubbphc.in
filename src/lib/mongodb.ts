16e0d0307db30d8fb613f533a850b75a3cdb4864
soupologist <saiashishvure@gmail.com>
2025-04-08 14:40:53 +0530

    fixed getFilm.ts

--- src/lib/mongodb.ts
+++ src/lib/mongodb.ts
@@ -6,26 +6,24 @@ const options = {};
 let client: MongoClient;
 let clientPromise: Promise<MongoClient>;
 
+// Augment the globalThis object to include _mongoClientPromise
+declare global {
+  var _mongoClientPromise: Promise<MongoClient> | undefined;
+}
+
+// Prevent TypeScript from treating this as a module
+export {};
+
 if (!process.env.MONGODB_URI) {
   throw new Error("Please add your MongoDB URI to .env.local");
 }
 
-// Correct usage of global typing
-declare global {
-  // Fix both errors here by using `let` not `var` and removing the eslint-disable
-  // `globalThis` is modern and valid for both Node and browser if needed
-  // You can also use `globalThis` in place of `global`
-  // Just make sure it’s consistent
-  // eslint-disable-next-line no-var -- ✘ REMOVE THIS LINE, it's not needed
-  let _mongoClientPromise: Promise<MongoClient> | undefined;
-}
-
 if (process.env.NODE_ENV === "development") {
-  if (!global._mongoClientPromise) {
+  if (!globalThis._mongoClientPromise) {
     client = new MongoClient(uri, options);
-    global._mongoClientPromise = client.connect();
+    globalThis._mongoClientPromise = client.connect();
   }
-  clientPromise = global._mongoClientPromise;
+  clientPromise = globalThis._mongoClientPromise;
 } else {
   client = new MongoClient(uri, options);
   clientPromise = client.connect();
